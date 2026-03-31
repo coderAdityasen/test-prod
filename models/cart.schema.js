8d40daf30af9e84a3e62b23e13811bcce2d3018e
Aditya sen <adityasen561@gmail.com>
2026-03-31 16:56:27 +0530

    update the cart models

--- models/cart.schema.js
+++ models/cart.schema.js
@@ -30,15 +30,77 @@ const cartSchema = new mongoose.Schema(
       type: Number,
       default: 0,
       min: [0, 'Total price cannot be negative']
-    },
-    isEmpty: {
-      type: Boolean,
-      default: true
     }
   },
   {
-    timestamps: true // Adds createdAt and updatedAt fields automatically
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
 );
+
+// Virtual for checking if cart is empty
+cartSchema.virtual('isEmpty').get(function () {
+  return this.items.length === 0;
+});
+
+// Virtual for item count
+cartSchema.virtual('itemCount').get(function () {
+  return this.items.reduce((sum, item) => sum + item.quantity, 0);
+});
+
+// Instance method to add item to cart
+cartSchema.methods.addItem = function (productId, quantity, price) {
+  const existingItem = this.items.find(item =>
+    item.productId.toString() === productId.toString()
+  );
+
+  if (existingItem) {
+    existingItem.quantity += quantity;
+  } else {
+    this.items.push({ productId, quantity, price });
+  }
+
+  this.updateTotal();
+  return this;
+};
+
+// Instance method to remove item from cart
+cartSchema.methods.removeItem = function (productId) {
+  this.items = this.items.filter(
+    item => item.productId.toString() !== productId.toString()
+  );
+  this.updateTotal();
+  return this;
+};
+
+// Instance method to clear cart
+cartSchema.methods.clearCart = function () {
+  this.items = [];
+  this.totalPrice = 0;
+  return this;
+};
+
+// Instance method to update total price
+cartSchema.methods.updateTotal = function () {
+  this.totalPrice = this.items.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+};
+
+// Static method to find cart by userId
+cartSchema.statics.findByUserId = function (userId) {
+  return this.findOne({ userId });
+};
+
+// Static method to create/get cart for user
+cartSchema.statics.getOrCreateCart = async function (userId) {
+  let cart = await this.findByUserId(userId);
+  if (!cart) {
+    cart = await this.create({ userId, items: [] });
+  }
+  return cart;
+};
 
 module.exports = mongoose.model('Cart', cartSchema);
